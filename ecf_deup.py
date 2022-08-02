1a88eae32a62f8cb94b462a69fc8d4948357a3f3
Mike Babb <mike7400@gmail.com>
2022-08-02 00:46:26 -0400

    Added country wide variant

--- ecf_deup.py
+++ ecf_deup.py
@@ -1,7 +1,17 @@
+import pathlib
+from datetime import datetime
+from typing import Optional
+
+import geopandas as gpd
 import pandas as pd
+import requests
 
 
-def map_bens(usac_df: pd.DataFrame, state: str = "NC"):
+def current_date():
+    return datetime.today().strftime("%Y-%m-%d")
+
+
+def svf_join(ecf_df: pd.DataFrame):
     svf_filepath = "data/2022 SVF from 2021 SVF from Oct 2019 Claim Data - SVF2022.csv"
     svf_df = pd.read_csv(svf_filepath)
     svf_df = svf_df.groupby(["District Entity Number"]).first().reset_index()
@@ -13,32 +23,80 @@ def map_bens(usac_df: pd.DataFrame, state: str = "NC"):
     regions_df = pd.read_csv(regions_path)
     regions_df["LEA No."] = regions_df["LEA No."].str.zfill(3)
 
-    usac_df = usac_df[usac_df["Billed Entity State"] == state]
-    usac_df = usac_df[
-        usac_df["Billed Entity Number (BEN)"].isin(svf_df["District Entity Number"])
+    ecf_df = ecf_df[ecf_df["Billed Entity State"] == "NC"]
+    ecf_df = ecf_df[
+        ecf_df["Billed Entity Number (BEN)"].isin(svf_df["District Entity Number"])
     ]
 
-    usac_df = pd.merge(
-        usac_df,
+    ecf_df = pd.merge(
+        ecf_df,
         svf_df[["District Entity Number", "State School ID", "LEA Number"]],
         left_on="Billed Entity Number (BEN)",
         right_on="District Entity Number",
         how="inner",
     )
 
-    usac_df = pd.merge(
-        usac_df,
+    ecf_df = pd.merge(
+        ecf_df,
         regions_df[["LEA No.", "SBE Region", "SBE Region Names"]],
         how="left",
         left_on="LEA Number",
         right_on="LEA No.",
     ).drop(["LEA No."], axis=1)
 
-    return usac_df
+    return ecf_df
 
 
-def dedup(usac_df: pd.DataFrame):
-    frns = usac_df.groupby(["Funding Request Number (FRN)", "FRN Line Item ID"])
+def map_bens(ecf_df: pd.DataFrame):
+    supp_path = "data/E-Rate_Supplemental_Entity_Information.csv"
+    supp_df = pd.read_csv(supp_path)
+
+    # no_lea_ixs = supp_df["State Local Education Agency (LEA) Code"].isnull()
+    # lea = supp_df[~no_lea_ixs]
+
+    # def map_pen(pen: str):
+    #     if not pd.isna(pen):
+    #         row = lea[lea["Entity Number"] == pen]
+    #         if len(row) > 0:
+    #             return row["State Local Education Agency (LEA) Code"].iloc[0]
+    #     return pd.NA
+
+    # supp_df.loc[no_lea_ixs, "State Local Education Agency (LEA) Code"] = supp_df.loc[
+    #     no_lea_ixs, "Parent Entity Number"
+    # ].apply(map_pen)
+
+    supp_df = (
+        supp_df.groupby("Entity Number", as_index=False).last().reset_index(drop=True)
+    )
+
+    ecf_df = pd.merge(
+        ecf_df,
+        supp_df,
+        left_on="Billed Entity Number (BEN)",
+        right_on="Entity Number",
+        how="left",
+    )
+
+    return ecf_df
+
+
+def spatial_join(df: pd.DataFrame):
+    gdf = gpd.GeoDataFrame(
+        df, geometry=gpd.points_from_xy(df.Longitude, df.Latitude), crs="EPSG:4269"
+    )
+
+    school_districts_path = "~/Documents/My Tableau Repository/Datasources/EDGESCHOOLDISTRICT_TL21_SY2021/schooldistrict_sy2021_tl21.shp"
+    school_districts_gdf = gpd.read_file(school_districts_path)
+
+    gdf = gdf.sjoin(school_districts_gdf, how="left")
+
+    df = pd.DataFrame(gdf.drop(columns="geometry"))
+
+    return gdf
+
+
+def dedup(ecf_df: pd.DataFrame):
+    frns = ecf_df.groupby(["Funding Request Number (FRN)", "FRN Line Item ID"])
 
     drop_ixs = []
 
@@ -50,17 +108,9 @@ def dedup(usac_df: pd.DataFrame):
             pending_ixs = statuses[statuses == "Pending"].index
             drop_ixs.extend(pending_ixs)
 
-    usac_df = usac_df.drop(drop_ixs, axis=0)
-
-    n = 0
+    ecf_df = ecf_df.drop(drop_ixs, axis=0)
 
     def split_firms(x: str):
-        nonlocal n
-        n += 1
-
-        if pd.isna(x):
-            return x, x
-
         items = x.split("},")
 
         names, numbers = [], []
@@ -74,20 +124,51 @@ def dedup(usac_df: pd.DataFrame):
 
         return ", ".join(names), ", ".join(numbers)
 
+    firm_ixs = ~ecf_df["Consulting Firm"].isnull()
+    firms = ecf_df.loc[firm_ixs, "Consulting Firm"]
+
     (
-        usac_df["Consulting Firm Names"],
-        usac_df["Consulting Firm Numbers"],
-    ) = zip(*usac_df["Consulting Firm"].apply(split_firms, 1))
+        ecf_df.loc[firm_ixs, "Consulting Firm Names"],
+        ecf_df.loc[firm_ixs, "Consulting Firm Numbers"],
+    ) = zip(*firms.apply(split_firms))
 
-    return usac_df
+    return ecf_df
 
 
-usac_filepath = "data/ECF_NC_Only.csv"
+def get_ecf_data():
+    url = "https://opendata.usac.org/api/views/i5j4-3rvr/rows.csv?accessType=DOWNLOAD"
+    r = requests.get(url)
 
-usac_df = pd.read_csv(usac_filepath)
+    ecf_filepath = f"data/ECF Data - {current_date()}.csv"
+    ecf_filepath = pathlib.Path(ecf_filepath)
 
-usac_df = map_bens(usac_df)
+    if not ecf_filepath.exists():
+        with open(ecf_filepath, "wb") as file:
+            file.write(r.content)
 
-usac_df = dedup(usac_df)
+    return ecf_filepath
 
-usac_df.to_csv("data/ECF Dedup.csv", index=False)
+
+def main(ecf_filepath: Optional[str] = None):
+    if ecf_filepath is None:
+        ecf_filepath = get_ecf_data()
+
+    ecf_filepath = pathlib.Path(ecf_filepath)
+    out_filepath = ecf_filepath.with_name(f"{ecf_filepath.stem} - Deduped.csv")
+
+    ecf_df = pd.read_csv(ecf_filepath)
+
+    ecf_df = map_bens(ecf_df)
+    ecf_df = spatial_join(ecf_df)
+    # NC only
+    ecf_df = svf_join(ecf_df)
+    ecf_df = dedup(ecf_df)
+
+    ecf_df.to_csv(out_filepath, index=False)
+
+
+if __name__ == "__main__":
+    # ecf_filepath = "data/ECF_NC_Only.csv"
+    ecf_filepath = "data/Emergency_Connectivity_Fund_FCC_Form_471 - July 30 - USA.csv"
+
+    main()
