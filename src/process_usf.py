39cd8a19f535d61379ee9209144cf296af4deffd
Mike Babb <mike7400@gmail.com>
2023-05-10 11:38:12 -0400

    minor

--- src/process_usf.py
+++ src/process_usf.py
@@ -1,5 +1,6 @@
 import pathlib
 from typing import Any
+
 import pandas as pd
 from googleapiutils2 import Sheets, get_oauth2_creds
 
@@ -12,20 +13,45 @@ HEADER = [
     "Schools & Libraries",
     "Rural Healthcare",
     "Total Subsidies",
-    "% of Total",
+    "% of Total Subsidies",
     "Contributions",
-    "% of Total",
+    "% of Total Contributions",
     "Delta",
+]
+DOLLAR_HEADER = [
+    "High Cost",
+    "Low Income",
+    "Schools & Libraries",
+    "Rural Healthcare",
+    "Total Subsidies",
+    "Contributions",
+    "Delta",
+]
+
+SUBSIDIES_HEADER = [
+    "High Cost",
+    "Low Income",
+    "Schools & Libraries",
+    "Rural Healthcare",
 ]
 
 HEADER_MARKER = "% of Total"
 
 TABLE = "1.9"
 
+YEARS_IN_THOUSANDS = [2022, 2021, 2018, 2017, 2016, 2015]
+
+
+def dollar_to_float(value: Any) -> float:
+    try:
+        return float(value.replace("$", "").replace(",", ""))
+    except:
+        return 0.0
+
 
 def find_first(df: pd.DataFrame, value: str) -> int | None:
     for ix, row in df.iterrows():
-        if any(value in str(cell) for cell in row):
+        if any(value.lower() in str(cell).lower() for cell in row):
             return ix
     return None
 
@@ -34,7 +60,7 @@ def process_dir(dir_path: pathlib.Path):
     dfs = []
 
     for file in dir_path.glob("**/*.xlsx"):
-        if "Section 1" not in file.name:
+        if "Section 1" not in file.name or "~$" in file.name:
             continue
         year = int(file.parent.name)
 
@@ -66,8 +92,26 @@ def process_dir(dir_path: pathlib.Path):
         df = df.iloc[:, : len(HEADER)]
         df.columns = HEADER
 
+        df[DOLLAR_HEADER] = df[DOLLAR_HEADER].astype(float)
+
+        if year in YEARS_IN_THOUSANDS:
+            df[DOLLAR_HEADER] *= 1000.0
+            if year == 2017:
+                df[["High Cost", "Low Income"]] /= 1000.0
+
         df["Year"] = year
         df["State Import"] = df["Delta"] / df["Contributions"]
+
+        df["Total Subsidies"] = df[SUBSIDIES_HEADER].sum(axis=1)
+
+        df["% of Total Subsidies"] = df["Total Subsidies"] / df["Total Subsidies"].sum()
+
+        df["% of Total Contributions"] = df["Contributions"] / df["Contributions"].sum()
+
+        df["Delta"] = df["Total Subsidies"] - df["Contributions"]
+
+        df["State Import"] = df["Delta"] / df["Contributions"]
+        df["Net Retention"] = 1 + df["State Import"]
 
         # sort df by state:
         df = df.sort_values(["State"], ascending=[True]).reset_index(drop=True)
@@ -87,6 +131,8 @@ def process_dir(dir_path: pathlib.Path):
 def join_ecf(usf_df: pd.DataFrame, ecf_filepath: pathlib.Path):
     ecf_df = pd.read_csv(ecf_filepath)
 
+    ecf_df = ecf_df[ecf_df["Funding Request Status"] == "Funded"]
+
     ecf_df = (
         ecf_df.groupby(["Billed Entity State"])
         .agg({"Line Total Cost": "sum"})
@@ -94,31 +140,15 @@ def join_ecf(usf_df: pd.DataFrame, ecf_filepath: pathlib.Path):
     )
     ecf_df["Year"] = 2022
 
-    us_states_names_path = pathlib.Path("data/us-states-names.csv")
-
-    us_states_names_df = pd.read_csv(us_states_names_path)
-    us_states_names_df = us_states_names_df.rename(
-        columns={"Name": "State Name", "Abbreviation": "State Abbreviation"}
-    )
-    usf_df = usf_df.merge(
-        us_states_names_df, how="left", left_on="State", right_on="State Name"
-    )
-
     usf_df = usf_df.merge(
         ecf_df,
         how="left",
         left_on=["State Abbreviation", "Year"],
         right_on=["Billed Entity State", "Year"],
     )
+    usf_df = usf_df.drop(columns=["Billed Entity State"])
 
     return usf_df
-
-
-def dollar_to_float(value: Any) -> float:
-    try:
-        return float(value.replace("$", "").replace(",", ""))
-    except:
-        return 0.0
 
 
 def join_acp(usf_df: pd.DataFrame, acp_filepath: pathlib.Path):
@@ -147,6 +177,17 @@ def join_acp(usf_df: pd.DataFrame, acp_filepath: pathlib.Path):
     return usf_df
 
 
+def join_state_names(usf_df: pd.DataFrame, us_states_names_path: pathlib.Path):
+
+    us_states_names_df = pd.read_csv(us_states_names_path)
+    us_states_names_df = us_states_names_df.rename(
+        columns={"Name": "State Name", "Abbreviation": "State Abbreviation"}
+    )
+    return usf_df.merge(
+        us_states_names_df, how="left", left_on="State", right_on="State Name"
+    )
+
+
 if __name__ == "__main__":
     creds = get_oauth2_creds(client_config=pathlib.Path("auth/creds.json"))
     sheets = Sheets(creds)
@@ -154,11 +195,10 @@ if __name__ == "__main__":
     dir_path = pathlib.Path("./data/usf")
     df = process_dir(dir_path=dir_path)
 
-    filepath = pathlib.Path("data/USF Data.csv")
-    df.to_csv(filepath, index=False)
+    us_states_names_path = pathlib.Path("data/us-states-names.csv")
+    df = join_state_names(usf_df=df, us_states_names_path=us_states_names_path)
 
     ecf_filepath = pathlib.Path("data/ECF Deduped.csv")
-
     df = join_ecf(usf_df=df, ecf_filepath=ecf_filepath)
 
     acp_filepath = pathlib.Path(
@@ -170,3 +210,4 @@ if __name__ == "__main__":
     sheets.update(
         spreadsheet_id=SHEET_URL, range_name="Sheet1", values=sheets.from_frame(df)
     )
+    sheets.resize_columns(spreadsheet_id=SHEET_URL, sheet_name="Sheet1", width=120)
